9f6b4b0a2ece7429314160231466b1a8c9fcf16e
VeronikaPoteichuk <nikaapots@gmail.com>
2025-09-16 13:56:23 +0400

    Updated

--- TestApp/authtest/api.py
+++ TestApp/authtest/api.py
@@ -1,7 +1,5 @@
-from rest_framework import permissions, viewsets
 from django.contrib.auth.models import User
 from .models import Profile
-from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import serializers
 
